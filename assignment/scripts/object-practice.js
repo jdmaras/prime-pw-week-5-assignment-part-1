13c9706ab0fd77cfd6aa6e085f7f81832db2bf30
John Maras <sportsballenthusiast@gmail.com>
2022-04-28 13:45:53 -0600

    Answered Questions 3 and 5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -8,12 +8,13 @@ console.log("***** Object Practice *****");
   - Give it  a numeric 'shoeCount' property
   - Also give it an array 'favThreeFoods'
 */
-const me = {
+let me = {
   firstName: "John",
   lastName: "Maras",
   shoeCount: 8,
-  favThreeFoods: ["dumplings", "shrimp", "spaghetti"],
   hasSiblings: true,
+  favThreeFoods: ["dumplings", "shrimp", "spaghetti"],
+
   // TODO - add properties here
 };
 console.log("A little about me:", me);
@@ -24,13 +25,16 @@ console.log("A little about me:", me);
     created above to set its value. 
   - Console.log fullName
 */
-let fullName = me.firstName + " " + me.lastName;
+let fullName = `${me.firstName} ${me.lastName}`;
 console.log(fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
+// I want to access three fav foods and then I'm giving and index. It's checking through that and the minus one starts me at the end
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -38,6 +42,15 @@ console.log(fullName);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log(me.shoeCount);
+me.shoeCount = me.shoeCount + 1;
+
+/* me.shoeCount += 1;
+This is a shorthand version that is doing the shoecount plus and equal to one more
+*/
+
+// i want the value of me.shoecount to equal that plus 1
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -45,3 +58,5 @@ console.log(fullName);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+me.favoriteColor = "plum";
+console.log(me.favoriteColor);
